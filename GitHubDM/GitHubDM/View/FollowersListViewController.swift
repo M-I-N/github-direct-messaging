797297a9e3f787af9df76c73c057b2add39d5acd
Mufakkharul Islam Nayem <mufakkharul.nayem@bjitgroup.com>
2019-09-11 15:08:03 +0600

    Create Xcode project.

---
+++ GitHubDM/GitHubDM/View/FollowersListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FollowersListViewController.swift
+//  GitHubDM
+//
+//  Created by Mufakkharul Islam Nayem on 9/11/19.
+//  Copyright © 2019 Mufakkharul Islam Nayem. All rights reserved.
+//
+
+import UIKit
+
+class FollowersListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
