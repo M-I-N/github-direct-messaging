5fb438177c56594c2cec5710e859ae046cedd40b
Mufakkharul Islam Nayem <mufakkharul.nayem@bjitgroup.com>
2019-09-12 15:22:10 +0600

    Change Messaging View Controller initialization
    - Messaging storyboard added
    - MessagingViewController is loaded from storyboard instead of pure programmatic approach
    - Initialization from storyboard is taking help from a new protocol StoryboardInstantiable

--- GitHubDM/GitHubDM/Screens/DirectMessaging/View/MessagingViewController.swift
+++ GitHubDM/GitHubDM/Screens/DirectMessaging/View/MessagingViewController.swift
@@ -10,16 +10,7 @@ import UIKit
 
 class MessagingViewController: UIViewController {
 
-    private let viewModel: MessagingViewModel
-
-    init(viewModel: MessagingViewModel) {
-        self.viewModel = viewModel
-        super.init(nibName: nil, bundle: nil)
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    var viewModel: MessagingViewModel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,4 +18,12 @@ class MessagingViewController: UIViewController {
         navigationItem.largeTitleDisplayMode = .never
     }
 
+}
+
+extension MessagingViewController: StoryboardInstantiable {
+
+    static var storyboardName: StoryboardName {
+        return .messaging
+    }
+    
 }
