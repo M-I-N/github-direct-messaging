1084339287ece060e2931f83b0a48809caef934c
Mufakkharul Islam Nayem <nil.nayem@gmail.com>
2019-09-15 01:09:42 +0600

    Handle an edge case when tableview scroll to bottom may cause a crash if called without any item has already been added
    - Bail out of the scrollToBottom function if there is no section or no row available

--- GitHubDM/GitHubDM/Protocols+Extensions/UITableView+Scrolling.swift
+++ GitHubDM/GitHubDM/Protocols+Extensions/UITableView+Scrolling.swift
@@ -13,11 +13,16 @@ extension UITableView {
     /// Scrolls to the bottom of the table view. Gets executed on the main queue.
     func scrollToBottom() {
         DispatchQueue.main.async {
-            let section = self.numberOfSections - 1
-            let safeSection = section < 0 ? 0 : section
-            let row = self.numberOfRows(inSection: safeSection) - 1
-            let safeRow = row < 0 ? 0 : row
-            let indexPath = IndexPath(row: safeRow, section: safeSection)
+            // bail out if there is no section in the table view
+            guard self.numberOfSections > 0 else { return }
+            // find the last section
+            let lastSection = self.numberOfSections - 1
+            // bail out if there is no row in the last section
+            guard self.numberOfRows(inSection: lastSection) > 0 else { return }
+            // find the last row of the last section
+            let lastRow = self.numberOfRows(inSection: lastSection) - 1
+            
+            let indexPath = IndexPath(row: lastRow, section: lastSection)
             self.scrollToRow(at: indexPath, at: .bottom, animated: true)
         }
     }
